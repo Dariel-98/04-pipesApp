25a809541950d854fcb65dd10f7b416a537d8bb7
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-10-30 16:02:44 -0600

    I18nPlural Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,10 +6,19 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class NoComunesComponent {
+  // i18nSelect
   nombre: string = 'Dariel';
   genero: string = 'masculino';
   invitacionMapa = {
     masculino: 'invitarlo',
     femenino: 'invitarla',
+  };
+
+  // i18Plural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'un cliente esperando',
+    other: 'tenemos # clientes esperando.',
   };
 }
