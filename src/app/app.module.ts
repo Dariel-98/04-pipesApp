9b4a46ff0b0cc268ed4e4cae45d9c64d296e313d
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-10-30 07:52:43 -0600

    PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+
+// Modulo personalizado
 import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouterModule, SharedModule],
+  imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
   providers: [],
   bootstrap: [AppComponent],
 })
