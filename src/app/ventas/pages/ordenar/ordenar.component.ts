f1ac7854237d9a94951e593419d9c368137b80c4
Dariel-Gomez <gomez.b.dariel@gmail.com>
2023-10-31 07:01:44 -0600

    Personalized pipes - capitalized pipe

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class OrdenarComponent {
+  valor: boolean = true;
 
+  enMayusculas() {
+    this.valor = !this.valor;
+    // if (this.valor) {
+    //   this.valor = false;
+    // } else {
+    //   this.valor = true;
+    // }
+  }
 }
